88cbd083860d17f1c35d9d589f32edafa1cc2471
Mm9450 <manas1999mishra@gmail.com>
2022-02-03 13:10:12 +0530

    added

---
+++ countFreq.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void countFreq(int arr[],int n){
+    vector<bool>visited(n,false);
+
+    for(int i=0; i<n;i++){
+        if(visited[i]==true)
+        continue;
+
+        int count =1;
+        for(int j=i+1; j<n;j++){
+            if(arr[i]==arr[j]){
+                visited[j]==true;
+                count++;
+            }
+        }
+        cout<<arr[i]<<" "<<count<<endl;
+    }
+}
+
+
+
+
+
+
+int main(){
+    int arr[]={2,2,3,3,4,5,6,6};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    countFreq(arr,n);
+
+
+    return 0;
+}
