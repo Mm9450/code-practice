0faaff87312a43130a1a19e38dd458e19c83a668
Mm9450 <manas1999mishra@gmail.com>
2022-02-05 11:29:07 +0530

    added

--- COUNTmisses.cpp
+++ COUNTmisses.cpp
@@ -1,15 +1,18 @@
+
 #include<iostream>
 using namespace std;
 int main(){
-    string s;
-    cin>>s;
+    string s = "aghafs@# jagds@# $# ";
+ //   cin>>s;
     int i=0,c=0;
     while(s[i]){
-        if(!('a'>=s[i]&&'z'<=s[i])||('A'>=s[i]&&'Z'<=s[i])||(' '==s[i])||('0'>=s[i]&&'9'<=s[i]))
-        c++;
+       // if(!((s[i]>='a'&& s[i]<='z')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='0'&&s[i]<='9')||(s[i]==' ')))
+        if((int(s[i])>=32&&int(s[i])<=47|| (s[i])>=58 &&int(s[i])<=64||int(s[i])>= 91&&int(s[i])<= 96 ||int(s[i])>= 123&&int(s[i])<= 126) && int(s[i]) != 32){
+              c++;
+              cout<<s[i]<<endl;
+        }
         i++;
     }
 
     cout<<c<<endl;
-
 }
