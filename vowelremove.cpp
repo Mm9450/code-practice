b10641d7504125781ae97d9bd07a7526d06fb751
Mm9450 <manas1999mishra@gmail.com>
2022-02-12 19:56:34 +0530

    added

--- vowelremove.cpp
+++ vowelremove.cpp
@@ -6,17 +6,18 @@ void removeVowel(char *str){
     int count =0;
     for(int i=0;str[i];i++)
     {   
-      if(str[i]!='a'||str[i]!='e'||str[i]!='i'||str[i]!='o'||str[i]!='u'||str[i]!='A'||str[i]!='E'||str[i]!='I'||str[i]!='O'||str[i]!='U'){
+       if(str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u'&&str[i]!='A'&&str[i]!='E'&&str[i]!='I'&&str[i]!='O'&&str[i]!='U'&&str[i]!= ' ')
+       {
           str[count++]=str[i];
        }
-       
+        
     }
-     str[count]='\0';
+    str[count]='\0';
 }
 
 
 int main(){
-    char str[] = "aeek gaeeiouks";
+    char str[] = "baba ramdev ";
 
     removeVowel(str);
     cout<<str;
