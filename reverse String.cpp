92540c4470c856b66910cb291219661a7bc88a83
Mm9450 <manas1999mishra@gmail.com>
2022-02-14 15:56:48 +0530

    added

---
+++ reverse String.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string str="hello";
+
+     reverse(str.begin(), str.end());
+  
+  cout << "The reversed string is- " << str;
+  return 0;
+
+
+    
+
+}
